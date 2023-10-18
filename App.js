7683ba5be684a1de8a48d9a336e917525aef70cc
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-17 22:00:49 -0700

    playing with styling

--- App.js
+++ App.js
@@ -4,9 +4,12 @@ import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 const App = () => {
 
   return (
-    <SafeAreaView style = {{ flex:1 }}>
+    <SafeAreaView style = { styles.wrapper }>
     <View style = {styles.container}>
       <Text>Hello world!</Text>
+      <Text>6</Text>
+      <Text style={ styles.feels }>Feels like 5</Text>
+      <Text>High : 8 Low : 6</Text>
     </View>
     </SafeAreaView>
   )
@@ -18,6 +21,14 @@ const styles = StyleSheet.create({
     backgroundColor: "orange",
     alignItems: "center",
     justifyContent: "center"
+  },
+  wrapper: {
+    flex: 1,
+    backgroundColor: "red"
+  },
+  feels: {
+    color: "white",
+    fontSize: 50,
   }
 })
 
