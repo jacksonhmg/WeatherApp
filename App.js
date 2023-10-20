5e84f9e2533cbf6534ab90059d22d062d360be59
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-19 17:39:59 -0700

    fixing simulator

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import { Entypo } from '@expo/vector-icons'; 
 import CurrentWeather from "./src/components/CurrentWeather";
+import UpcomingWeather from "./src/components/UpcomingWeather";
 
 const App = () => {
 
   return (
     <View style={ styles.container }>
+      <UpcomingWeather />
       <CurrentWeather />
     </View>
   )
--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, FlatList, View } from 'react-native';
 
 
-const UpcomingWeather = ({ weather }) => {
+const UpcomingWeather = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Text>Upcoming Weather</Text>
+            <FlatList
+                data={data}
+                renderItem={({ item }) => <Item day={item.name} temp={item.temp} />}
+                keyExtractor={item => item.id}
+            />
         </SafeAreaView>
     )
 }
@@ -15,5 +20,52 @@ const styles = StyleSheet.create({
         flex: 1,
     }
 })
+
+const Item = ( day, temp ) => {
+    return (
+        <View style={styles.container}>
+            <Text>{day}</Text>
+            <Text>{temp}</Text>
+        </View>
+    )
+}
+
+const data = [
+    {
+        id: 1,
+        name: "Monday",
+        temp: 6,
+    },
+    {
+        id: 2,
+        name: "Tuesday",
+        temp: 7,
+    },
+    {
+        id: 3,
+        name: "Wednesday",
+        temp: 8,
+    },
+    {
+        id: 4,
+        name: "Thursday",
+        temp: 9,
+    },
+    {
+        id: 5,
+        name: "Friday",
+        temp: 10,
+    },
+    {
+        id: 6,
+        name: "Saturday",
+        temp: 11,
+    },
+    {
+        id: 7,
+        name: "Sunday",
+        temp: 12,
+    }
+]
 
 export default UpcomingWeather;
